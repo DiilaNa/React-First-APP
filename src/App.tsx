0e23690f063c172c24f5b815be2afac6361e577b
DiilaNa <liyanaarachchidilan@gmail.com>
2025-09-15 10:21:14 +0530

    React Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import React, { Component } from 'react';
 
+//Functional Component
 
-function App() {
+//function App() {
 
-  // java script athule xml use karanna puluwan
-  // e.g. const element = <h1>Hello, world!</h1>;
+//   // java script athule xml use karanna puluwan
+//   // e.g. const element = <h1>Hello, world!</h1>;
 
-  return <h1>Hello, world!</h1>;
+//   return <h1>Hello, world!</h1>;
+// }
+
+//Class Component
+class App extends Component{ 
+  render(){
+    return <h1>Hello, world!</h1>;
+  }
 }
 
 export default App
