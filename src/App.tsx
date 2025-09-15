7e38223c0d1b18d86f8ab709e478377b33d53e9e
DiilaNa <liyanaarachchidilan@gmail.com>
2025-09-15 11:10:29 +0530

    Js inisde html using react

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,59 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import { Fragment } from "react/jsx-runtime";
+
+
+
+//Class Component
+// class App extends Component{ 
+//   render(){
+//     return <div>
+//       <h1>Hello, world!</h1>
+//       <p>This is a simple React class component.</p>
+//       <p>It demonstrates the use of JSX in a class-based component.</p>
+//       <p>Class components can hold and manage their own state.</p>
+//       </div>;
+//   }
+// }
 
 //Functional Component
 
-//function App() {
+function App() {
 
-//   // java script athule xml use karanna puluwan
-//   // e.g. const element = <h1>Hello, world!</h1>;
+  // java script athule xml use karanna puluwan
+  // e.g. const element = <h1>Hello, world!</h1>;
 
-//   return <h1>Hello, world!</h1>;
-// }
+  // For Wrap elements
+  // 1. <fragment> </fragment> - OLD
+  // 2. <div> </div> - MOST USED
+  // 3. <> </> - NEW
+   let value: string = "Dilan";
+  return(
+  //  <div>       
+  //   <h1>Hello, world!</h1>
+  //   <p>Functional components can hold and manage their own state.</p>
+  //   </div>
 
-//Class Component
-class App extends Component{ 
-  render(){
-    return <h1>Hello, world!</h1>;
-  }
+    // <Fragment>
+    //   <h1>Hello, world!</h1>
+    //   <p>Functional components can hold and manage their own state.</p>
+    // </Fragment>
+
+    // <>
+    //   <h1>Hello, world!</h1>
+    //   <p>Functional components can hold and manage their own state.</p>
+    
+    // </>
+
+    //We can use JavaScript expressions inside Html by wrapping them in curly braces {}
+    <>
+      <h1>{value}</h1> 
+      {value ? <h1>Hello,{value}</h1>: <h1>Hello, React</h1>}
+      {console.log(value)}
+      <h1>Hello, {value || "React"}</h1>
+      <h2>this is react</h2>
+      <h2>app component</h2>
+    </>
+  );
 }
 
 export default App
