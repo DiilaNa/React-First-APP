d88563e7f293d5a6d110df8892e11d7f0ec1e1ba
DiilaNa <liyanaarachchidilan@gmail.com>
2025-09-15 10:15:29 +0530

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+
+  // java script athule xml use karanna puluwan
+  // e.g. const element = <h1>Hello, world!</h1>;
+
+  return <h1>Hello, world!</h1>;
+}
+
+export default App
